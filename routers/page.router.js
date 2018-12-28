80738c5d38672a62c543793ad4059243ec99bd1d
Oussema Romdhane <babysiella@hotmail.com>
2018-12-28 19:43:37 +0100

    navbar/register/login

--- routers/page.router.js
+++ routers/page.router.js
@@ -2,4 +2,13 @@ const router = require("express").Router();
 router.get("/register", (req, res) => {
   res.render("register");
 });
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/acceuil", (req, res) => {
+  res.render("acceuil");
+});
+router.get("/apropos", (req, res) => {
+  res.render("apropos");
+});
 module.exports = router;
